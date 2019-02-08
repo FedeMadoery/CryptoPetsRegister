49ef9669d0c4e5dd53c89c284d35aef393593813
Puchoo <fede.madoery@hotmail.com>
2019-02-08 15:34:08 -0300

    Compile.js now export a .json to public folder

--- contracts/scripts/compile.js
+++ contracts/scripts/compile.js
@@ -1,9 +1,25 @@
 const path = require('path');
-const fs = require('fs');
+const fs = require('fs-extra');
 const solc = require('solc');
 
-let reqPath = path.join(__dirname, '../');
-const contractPath = path.resolve(reqPath, 'solidity-contracts', 'PetsManager.sol');
-const source = fs.readFileSync(contractPath, 'utf8');
+const basePath = path.join(__dirname, '../');
 
-module.exports = solc.compile(source, 1).contracts[':PetsManager'];
+const buildPath = path.resolve(basePath, 'public');
+fs.removeSync(buildPath);
+
+const contractPath = path.resolve(basePath, 'solidity-contracts', 'PetsOwnership.sol');
+const source = fs.readFileSync(contractPath, 'utf8');
+const output = solc.compile(source, 1).contracts;
+
+console.log(output);
+
+fs.ensureDirSync(buildPath);
+
+for (let contract in output) {
+    fs.outputJsonSync(
+        path.resolve(buildPath, contract.replace(':', '') + '.json'),
+        output[contract]
+    );
+}
+
+console.log('Compile finish');
